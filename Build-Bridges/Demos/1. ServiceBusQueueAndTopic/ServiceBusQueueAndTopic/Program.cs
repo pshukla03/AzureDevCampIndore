1c5134b6022f18c90e40debb0cc76a89a1fa2880
Pawan Shukla <pashukla@microsoft.com>
2015-06-18 18:52:09 +0530

    Updated One

--- Build-Bridges/Demos/1. ServiceBusQueueAndTopic/ServiceBusQueueAndTopic/Program.cs
+++ Build-Bridges/Demos/1. ServiceBusQueueAndTopic/ServiceBusQueueAndTopic/Program.cs
@@ -2,11 +2,15 @@
 
 namespace ServiceBusQueueAndTopic
 {
+    using Microsoft.Azure;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
     class Program
     {
         static void Main(string[] args)
         {
-            RunQueueDemo();
+            //RunQueueDemo();
             RunTopicDemo();
 
             Console.ReadLine();
@@ -16,11 +20,30 @@ namespace ServiceBusQueueAndTopic
         {
             #region Create Queue
 
-            // Create the queue if it does not exist already
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"));
+
+            const string queueName = "TestQueue";
+            var queueDescription = new QueueDescription(queueName)
+            {
+                MaxSizeInMegabytes = 5120,
+                DefaultMessageTimeToLive = new TimeSpan(0, 1, 0)
+            };
+
+            if (!namespaceManager.QueueExists(queueName))
+            {
+                namespaceManager.CreateQueue(queueDescription);
+            }
 
             #endregion
 
             #region Send message to Queue
+
+            var client = QueueClient.CreateFromConnectionString(
+        CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"), queueName);
+
+            var message = new BrokeredMessage("Test Message");
+            message.Properties["PartnerName"] = "Microsoft";
+            client.Send(message);
 
             // Get service bus queue client
 
@@ -33,6 +56,28 @@ namespace ServiceBusQueueAndTopic
             #endregion
 
             #region Read message from Queue
+
+            var options = new OnMessageOptions
+            {
+                AutoComplete = true,
+                AutoRenewTimeout = TimeSpan.FromMinutes(1)
+            };
+
+            client.OnMessage(queuedMessage =>
+            {
+                try
+                {
+                    Console.WriteLine("Message Id:" + queuedMessage.MessageId);
+                    Console.WriteLine("Message Body:" + queuedMessage.GetBody<string>());
+                    Console.WriteLine("Partner Name:" + queuedMessage.Properties["PartnerName"]);
+
+                    queuedMessage.Complete();
+                }
+                catch (Exception)
+                {
+                    queuedMessage.Abandon();
+                }
+            }, options);
 
             // Configure the callback options
 
@@ -48,19 +93,61 @@ namespace ServiceBusQueueAndTopic
         private static void RunTopicDemo()
         {
             #region Create Topic
-            // Create the topic if it does not exist already
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(
+    CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"));
+
+            const string topicName = "TestTopic";
+            var topicDescription = new TopicDescription(topicName)
+            {
+                MaxSizeInMegabytes = 5120,
+                DefaultMessageTimeToLive = new TimeSpan(0, 1, 0)
+            };
+
+            if (!namespaceManager.TopicExists(topicName))
+            {
+                namespaceManager.CreateTopic(topicDescription);
+            }
             #endregion
 
             #region Create Subscriptions
 
-            // Create subscription with default(Match All) filter
+            const string allMessagesSubscription = "AllMessages";
+            if (!namespaceManager.SubscriptionExists(topicName, allMessagesSubscription))
+            {
+                namespaceManager.CreateSubscription(topicName, allMessagesSubscription);
+            }
 
-            // Create subscription with Sql filters: HighMessages and LowMessages
+            const string highMessagesSubscription = "HighMessages";
+            if (!namespaceManager.SubscriptionExists(topicName, highMessagesSubscription))
+            {
+                var highMessagesFilter = new SqlFilter("MessageNumber > 3");
+                namespaceManager.CreateSubscription(topicName, highMessagesSubscription, highMessagesFilter);
+            }
+
+            const string lowMessagesSubscription = "LowMessages";
+            if (!namespaceManager.SubscriptionExists(topicName, lowMessagesSubscription))
+            {
+                var lowMessagesFilter = new SqlFilter("MessageNumber <= 3");
+                namespaceManager.CreateSubscription(topicName, lowMessagesSubscription, lowMessagesFilter);
+
+            }
+
 
             #endregion
 
             #region Send message to Topic
 
+            var client = TopicClient.CreateFromConnectionString(
+        CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"), topicName);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var topicMessage =
+                new BrokeredMessage("Test message " + i);
+                topicMessage.Properties["MessageNumber"] = i;
+                client.Send(topicMessage);
+
+            }
             // Get service bus topic client
 
             // Create message, passing a string message for the body
@@ -72,6 +159,40 @@ namespace ServiceBusQueueAndTopic
             #endregion
 
             #region Receive message from Topic
+
+            var subscriptionClientAll = SubscriptionClient.CreateFromConnectionString(
+    CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"), topicName,
+    allMessagesSubscription);
+
+            var subscriptionClientHigh = SubscriptionClient.CreateFromConnectionString(
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"), topicName,
+                highMessagesSubscription);
+
+            var subscriptionClientLow = SubscriptionClient.CreateFromConnectionString(
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString"), topicName,
+                lowMessagesSubscription);
+
+            var options = new OnMessageOptions
+            {
+                AutoComplete = false,
+                AutoRenewTimeout = TimeSpan.FromMinutes(1)
+            };
+
+            subscriptionClientAll.OnMessage(message =>
+            {
+                ProcessTopicMessage(message, allMessagesSubscription);
+            }, options);
+
+            subscriptionClientHigh.OnMessage(message =>
+            {
+                ProcessTopicMessage(message, highMessagesSubscription);
+            }, options);
+
+            subscriptionClientLow.OnMessage(message =>
+            {
+                ProcessTopicMessage(message, lowMessagesSubscription);
+            }, options);
+
 
             // Get subscription clients for all filters
 
@@ -85,5 +206,24 @@ namespace ServiceBusQueueAndTopic
 
             #endregion
         }
+
+        private static void ProcessTopicMessage(BrokeredMessage message, string messageSubscription)
+        {
+            try
+            {
+                Console.WriteLine("\n**{0}**", messageSubscription);
+                Console.WriteLine("Body: " + message.GetBody<string>());
+                Console.WriteLine("MessageID: " + message.MessageId);
+                Console.WriteLine("Message Number: " +
+                                    message.Properties["MessageNumber"]);
+
+                message.Complete();
+            }
+            catch (Exception)
+            {
+                message.Abandon();
+            }
+        }
+
     }
 }
